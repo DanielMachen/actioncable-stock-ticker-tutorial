a40fa9eadb34f3dad680c94b6f70214580bfa1ed
danielmachen <daniel@danielmachen.net>
2016-03-14 17:22:47 -0700

    Refining the ActionCable Portion

--- app/channels/stocks_channel.rb
+++ app/channels/stocks_channel.rb
@@ -1,16 +1,17 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class StocksChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "stock_stream:all_stocks"
+  def subscribed # Default stream removed
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def follow
+  def follow( data ) # Added data variable
+    stream_from "stock_stream:#{data['stock']}" # Added specific stock stream
   end
 
   def unfollow_all
+    stop_all_streams # Added call to 'stop_all_streams'
   end
 end
